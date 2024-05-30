52a7d62bb4ae5da887fd03c19c01d9297508e74f
Pat <59577400+patricks0510@users.noreply.github.com>
2024-05-30 16:07:24 -0400

    scrolling between pages now supported for cantrips. needs refactor once moving into different spell levels since cantrip json file hardcoded

--- scripts/cantrips.gd
+++ scripts/cantrips.gd
@@ -1,53 +1,79 @@
 extends Node2D
 
 @onready var displaySpell = [$SpellDisplay,$SpellDisplay2,$SpellDisplay3,
-$SpellDisplay4,$SpellDisplay5, $SpellDisplay6]
+$SpellDisplay4]
+
+@onready var spell_list_api = parse_spell_json()
+
+@export var firstSpell = 0
+@export var lastSpell = firstSpell + 4
 
 func _ready():
-	get_spell_text()
+	get_spell_text(spell_list_api)
 
 func _process(delta):
 	pass
-	
 
-func get_spell_text():
+func parse_spell_json():
 	var file = "res://imports/srd_cantrips.json"
 	var json_as_text = FileAccess.get_file_as_string(file)
 	var json_as_dict = JSON.parse_string(json_as_text)
 	var json_as_arr = json_as_dict.values()
 	var spells_api_text = json_as_arr.pop_back()
-	
-	for spell in range(0,6):
+	return spells_api_text
+
+func get_spell_text(spells_api_text):
+	for spell in range(0,4):
+		var spellToDisplay = spell + firstSpell
+		if(spellToDisplay>=spells_api_text.size()):
+			firstSpell -= 4
+			lastSpell -= 4
+			break
 		var displaySpell_children = displaySpell[spell].get_children()
-		var spellName = spells_api_text[spell].get("name")
+		var spellName = spells_api_text[spellToDisplay].get("name")
 		displaySpell_children[0].text = spellName
-		var description = spells_api_text[spell].get("desc")
+		var description = spells_api_text[spellToDisplay].get("desc")
 		displaySpell_children[1].text = description
-		var higherLevel = spells_api_text[spell].get("higher_level")
+		var higherLevel = spells_api_text[spellToDisplay].get("higher_level")
 		displaySpell_children[2].text = higherLevel
-		var range = spells_api_text[spell].get("range")
+		var range = spells_api_text[spellToDisplay].get("range")
 		displaySpell_children[3].text = range
-		var components = spells_api_text[spell].get("components")
+		var components = spells_api_text[spellToDisplay].get("components")
 		if components != null:
 			displaySpell_children[4].text = components
 		else:
 			displaySpell_children[4].text = "N/A"
-		var duration = spells_api_text[spell].get("duration")
+		var duration = spells_api_text[spellToDisplay].get("duration")
 		displaySpell_children[5].text = duration
-		var castTime = spells_api_text[spell].get("casting_time")
+		var castTime = spells_api_text[spellToDisplay].get("casting_time")
 		displaySpell_children[6].text = castTime
-		var level = spells_api_text[spell].get("level")
+		var level = spells_api_text[spellToDisplay].get("level")
 		displaySpell_children[7].text = level
-		var school = spells_api_text[spell].get("school")
+		var school = spells_api_text[spellToDisplay].get("school")
 		displaySpell_children[8].text = school
-		var classSpellList = spells_api_text[spell].get("dnd_class")
+		var classSpellList = spells_api_text[spellToDisplay].get("dnd_class")
 		displaySpell_children[9].text = classSpellList
-		var pageNum = spells_api_text[spell].get("page")
+		var pageNum = spells_api_text[spellToDisplay].get("page")
 		displaySpell_children[10].text = pageNum
-		var source = spells_api_text[spell].get("document__title")
+		var source = spells_api_text[spellToDisplay].get("document__title")
 		displaySpell_children[11].text = source
 	
 	#spells_api_text = spells_api_text[1]
 	#var spells_api_text_arr = spells_api_text.split("}, {",true)
 	#if spells_api_text:
 		#print(spells_api_text)
+
+
+func _on_back_pressed():
+	if(firstSpell == 0):
+		get_tree().change_scene_to_file("res://scenes/front_page.tscn")
+	else:
+		firstSpell -= 4
+		lastSpell -= 4
+		get_spell_text(spell_list_api)
+
+
+func _on_next_pressed():
+	firstSpell += 4
+	lastSpell += 4
+	get_spell_text(spell_list_api)
--- scripts/front_page.gd
+++ scripts/front_page.gd
@@ -3,4 +3,5 @@ extends Control
 
 func _on_cantrips_button_pressed():
 	#change scenes to cantrips.gd
+	get_tree().change_scene_to_file("res://scenes/cantrips.tscn")
 	pass # Replace with function body.
